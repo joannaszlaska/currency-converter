ca521beb7667995e88c2d320d3d1b70026be75ee
Joanna Szlaska <joanna.szlaska8@gmail.com>
2022-06-12 22:31:15 +0200

    js changed

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
 
-    let amount = amountElement.value;
-    let currency = currencyElement.value;
-    let EUR = 4.56;
-    let USD = 4.30;
-    let result;
+    const calculateResult = (amount, currency) => {
+        const EUR = 4.56;
+        const USD = 4.30;
 
-    switch (currency) {
-        case "EUR":
-            result = amount / EUR;
-            break;
+        switch (currency) {
+            case "EUR":
+                return amount / EUR;
 
-        case "USD":
-            result = amount / USD;
-            break
+            case "USD":
+                return amount / USD;
+        }
     }
-    resultElement.innerText = result.toFixed(2);
-});
+    
+    const updateResultText = (result) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerText = result.toFixed(2);
+    }
+    
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const amount = amountElement.value;
+        const currency = currencyElement.value;
+
+        let result = calculateResult(amount, currency);
+
+        updateResultText(result);
+
+    }
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+    
+    init();
+}
 
 
